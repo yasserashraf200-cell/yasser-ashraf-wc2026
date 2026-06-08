3d2ee352855423b08d87f0d093df73811aa59a66
Yasser Ashraf <yasserashraf200-cell@users.noreply.github.com>
2026-06-09 00:34:10 +0300

    Fix: Service worker POST error, deprecated meta tag

--- public/sw.js
+++ public/sw.js
@@ -32,6 +32,7 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
   if (event.request.url.includes('/api/')) {
     event.respondWith(
       fetch(event.request)
