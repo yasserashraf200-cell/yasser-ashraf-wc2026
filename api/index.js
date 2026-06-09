ce2f31b1006b10b049650c175eaf71f3847da737
Yasser Ashraf <yasserashraf200-cell@users.noreply.github.com>
2026-06-09 13:34:47 +0300

    Add root-level Vercel files: api/index.js, public/index.html, package.json

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
-
   try {
     const response = await axios.get('https://api.football-data.org/v4/competitions/2000/matches', {
       headers: { 'X-Auth-Token': process.env.FOOTBALL_API_KEY }
